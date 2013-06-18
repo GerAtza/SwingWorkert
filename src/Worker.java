ce5a86d65868b30657133c65366cd99cf6fcdf15
Gerardo Navarro <Gerardo Navarro@192.168.1.93>
2013-06-18 14:20:24 -0500

    SwingWorkert

---
+++ src/Worker.java
@@ -0,0 +1,49 @@
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+
+
+public class Worker extends SwingWorker<Double,Void> {
+
+	private JProgressBar progreso;
+	public boolean start;
+	
+	public Worker(JProgressBar bar){
+		progreso=bar;
+	}
+	
+	@Override
+	protected Double doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("doInBackGround() esta en el hilo "+Thread.currentThread().getName());
+		int val=this.progreso.getValue();
+		int max=this.progreso.getMaximum();
+		while(val<max){
+			//si start es true va ciclo-si no sigue en continue;
+			if(!start){
+				continue;
+			}
+			try{
+				Thread.sleep(100);
+			}catch(InterruptedException ex){
+				
+			}
+			
+			this.progreso.setValue(val+1);
+			val++;
+		}
+		return null;
+	}
+
+	@Override
+	protected void done() {
+		// TODO Auto-generated method stub
+		double siHaTerminado=this.progreso.getPercentComplete();
+		if(siHaTerminado==1.0){
+			WorkerFrame.txtResultadoWorker.setText("Termidao");
+		}
+		
+	}
+	
+	
+	
+}
